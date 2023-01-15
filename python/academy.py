d6ac3c25f2f333ecd16fac47e896a19cd11b363d
mah1ru0 <masevah@gmail.com>
2023-01-15 15:56:31 -0800

    Progress through the functions_exercises.py file Moved launch.json

--- python/academy.py
+++ python/academy.py
@@ -1,31 +1,7 @@
-hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+print("Checking the weather for you!")
 
-prices = [30, 25, 40, 20, 20, 35, 50, 35]
+def weather_check():
+  print("Looks great outside! Enjoy your trip.")
+  print("False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.")
 
-last_week = [2, 3, 5, 8, 4, 4, 6, 2]
-
-total_price = 0
-
-for price in prices:
-  total_price += price
-
-num_prices = len(prices)
-average_price = total_price / num_prices
-
-print("Average Haircut Price:", str(average_price))
-
-new_prices = [price -5 for price in prices]
-#print(new_prices)
-
-total_revenue = 0
-
-for i in range(0, len(hairstyles)):
-  total_revenue = (total_revenue + (prices[i] * last_week[i]))
-  #print(total_revenue)
-print("Total Revenue:", total_revenue)
-
-average_daily_revenue = total_revenue / 7
-print("Average Daily Revenue" , average_daily_revenue)
-
-cuts_under_30 = [hairstyles[i] for i in range(len(hairstyles)) if new_prices[i] < 30]
-print(cuts_under_30)
+weather_check()
--- python/functions_exercises.py
+++ python/functions_exercises.py
@@ -1,12 +1,7 @@
-#"""
+print("Checking the weather for you!")
 
-def directions_to_timesSq():
-  print("Walk 4 mins to 34th St Herald Square train station")
-  print("Take the Northbound N, Q, R, or W train 1 stop")
-  print("Get off the Times Square 42nd Street stop")
-  print("Take lots of pictures!")
+def weather_check():
+  print("Looks great outside! Enjoy your trip.")
+  print("False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.")
 
-directions_to_timesSq()
-
-
-#"""
+weather_check()
