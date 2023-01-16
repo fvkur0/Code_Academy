508552e30cb534c167aed1d9c171d44f8af79efc
fvkur0 <pat.myers91@gmail.com>
2023-01-16 11:17:34 -0800

    Finished Exercises

--- python/functions_exercises.py
+++ python/functions_exercises.py
@@ -61,3 +61,83 @@ print(rounded_price)
 
 """
 
+"""
+
+# This function will print a hardcoded count of how many locations we have.
+
+favorite_locations = "Paris, Norway, Iceland"
+
+def print_count_locations():
+    print("There are 3 locations")
+    
+# This function will print the favorite locations
+def show_favorite_locations():
+  print("Your favorite locations are: " + favorite_locations)
+
+print_count_locations()
+show_favorite_locations()
+
+"""
+
+"""
+
+current_budget = 3500.75
+
+def print_remaining_budget(budget):
+  print("Your remaining budget is: $" + str(budget))
+
+print_remaining_budget(current_budget)
+
+# Write your code below: 
+
+def deduct_expense(budget, expense):
+    return budget - expense
+
+shirt_expense = 9
+new_budget_after_shirt = deduct_expense(current_budget, shirt_expense)
+
+print_remaining_budget(new_budget_after_shirt)
+
+"""
+
+"""
+
+def top_tourist_locations_italy():
+  first = "Rome"
+  second = "Venice"
+  third = "Florence"
+  return first, second, third
+
+most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
+
+print(most_popular1)
+print(most_popular2)
+print(most_popular3)
+
+"""
+
+"""
+
+def trip_planner_welcome(name):
+    print("Welcome to tripplanner v1.0", name)
+
+name = "Pat"
+
+trip_planner_welcome(name)
+
+def estimated_time_rounded(estimated_time):
+    rounded_time = round(estimated_time)
+    return rounded_time 
+
+estimate = estimated_time_rounded(4.20)
+#print(estimate)
+
+def destination_setup(origin, destination, estimated_time, mode_of_transport = "Car"):
+    print("Your trip starts off in " + origin)
+    print("And you are traveling to " + destination)
+    print("You will be traveling by " + mode_of_transport)
+    print("It will take approximately " + str(estimated_time) + " hours")
+
+destination_setup("LA", "Sac", estimate, "Car")
+
+"""
