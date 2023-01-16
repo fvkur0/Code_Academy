6580f597c4467a24f24f2c05605e494f51276b08
fvkur0 <pat.myers91@gmail.com>
2023-01-15 23:04:40 -0800

    Created the void

--- python/academy.py
+++
@@ -1,7 +0,0 @@
-print("Checking the weather for you!")
-
-def weather_check():
-  print("Looks great outside! Enjoy your trip.")
-  print("False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.")
-
-weather_check()
