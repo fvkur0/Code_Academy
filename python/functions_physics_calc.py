42ac87c91fe7cc62c4763d5ce70af7122dbc5807
fvkur0 <pat.myers91@gmail.com>
2023-01-16 12:19:48 -0800

    Completed the functions Lab

---
+++ python/functions_physics_calc.py
@@ -0,0 +1,36 @@
+#End of functions lessons project
+
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+
+def f_to_c(f_temp):
+    c_temp = f_temp - 32
+    return c_temp
+f100_in_celcius = f_to_c(100)
+
+def c_to_f(c_temp):
+    f_temp = c_temp * (9/5) + 32
+    return f_temp
+c0_in_fahrenheit = c_to_f(0)
+
+print(f100_in_celcius)
+
+def get_force(mass, acceleration):
+    return mass * acceleration
+train_force = get_force(train_mass, train_acceleration)
+print("The GE train supplies " + str(train_force) + " Newtons of force.")
+
+def get_energy(mass):
+    c = 3*10**8
+    return mass * c ** 2
+
+bomb_energy = get_energy(bomb_mass)
+print("A 1kg bomb supplies " + str(bomb_energy) + " Joules")
+
+def get_work(distance):
+    force = get_force(train_mass, train_acceleration) * distance
+    return force
+train_work = get_work(train_distance)
+print("The GE train does " + str(train_work) + " Joules of work over " + str(train_distance) + " meters.")
