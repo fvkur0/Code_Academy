1972068109430afaec20b488582fe82245fc7d91
fvkur0 <pat.myers91@gmail.com>
2023-01-26 11:19:41 -0800

    Updated Progress

---
+++ python/strings_intro.py
@@ -0,0 +1,35 @@
+"""
+
+first_name = "Rodrigo"
+last_name = "Villanueva"
+
+new_account = last_name[:5]
+temp_password = last_name[5:]
+
+"""
+
+"""
+first_name = "Reiko"
+last_name = "Matsuki"
+
+def password_generator(first_name, last_name):
+    length1 = len(first_name)
+    length2 = len(last_name)
+    firs3 = first_name[length1-3:]
+    las3 = last_name[length2-3:]
+    print(firs3 + las3)
+
+password_generator("Reiko", "Matsuki")
+
+# ""  CA Solution:
+def password_generator(first_name, last_name):
+  temp_password = first_name[len(first_name)-3:] + last_name[len(last_name)-3:]
+  return temp_password
+
+temp_password = password_generator(first_name, last_name)
+
+"""
+
+
+
+"""
