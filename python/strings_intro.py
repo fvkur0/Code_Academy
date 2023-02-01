c64c64199474476f58468110400420822d46899b
fvkur0 <pat.myers91@gmail.com>
2023-02-01 11:16:34 -0800

    Progress

--- python/strings_intro.py
+++ python/strings_intro.py
@@ -30,6 +30,17 @@ temp_password = password_generator(first_name, last_name)
 
 """
 
-
-
 """
+
+company_motto = "Copeland's Corporate Company helps you capably cope with the constant cacophony of daily life"
+
+second_to_last = company_motto[-2]
+final_word = company_motto[-4:]
+
+#"""
+
+#"""
+
+
+
+#"""
