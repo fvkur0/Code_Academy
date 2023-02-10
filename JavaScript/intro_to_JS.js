a35ba98fde390eec8ee093bb728245d6ff00827a
fvkur0 <pat.myers91@gmail.com>
2023-02-09 21:48:11 -0800

    Progress

--- JavaScript/intro_to_JS.js
+++ JavaScript/intro_to_JS.js
@@ -1,8 +1,9 @@
 /* This is intro to JavaScript. Here I will keep the exercises from my procession along the lessons.
 I wil store all of the special projects in their own respective instances. */
 
+/*
+// Opening line
 console.log('It was love at first sight.');
-
 console.log('The first time Yossarian saw the chaplain he fell madly in love with him.');
 console.log('Yossarian was in the hospital with a pain in his liver that fell just short of being jaundice.');
 console.log('The doctors were puzzled by the fact that it wasn\'t quite jaundice.');
@@ -10,4 +11,51 @@ console.log('If it became jaundice they could treat it.');
 console.log('If it didn\'t become jaundice and went away they could discharge him.');
 console.log('But this just being short of jaundice all the time confused them.');
 
-console.log('This is a secondary test')
+-----------------------------------------
+// Primitive Types
+
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49) // Float
+
+-----------------------------------------
+// Operators
+
+console.log(31 + 3.5);
+console.log(2023 - 1969);
+console.log(65 / 240);
+console.log(.2708 * 100);
+
+-----------------------------------------
+// String Concatenation
+
+console.log('Hello' + 'World');
+console.log('Hello'+ ' ' + 'World');
+
+-----------------------------------------
+// Properties
+
+console.log('Teaching the world how to code'.length); //Prints 30
+
+-----------------------------------------
+// Methods
+
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+-----------------------------------------
+*/ 
+
