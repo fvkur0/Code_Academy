a7af4220709b095495e539fe94a2d77347896f14
fvkur0 <pat.myers91@gmail.com>
2023-02-10 02:29:35 -0800

    Completed

--- python/strings_intro.py
+++ python/strings_intro.py
@@ -39,8 +39,72 @@ final_word = company_motto[-4:]
 
 #"""
 
+"""
+
+def get_length(string):
+    count = 0
+    for letters in string:
+        count += 1
+        #print(count)
+    print(count)
+
+get_length("corruption")
+
 #"""
 
+"""
 
+def letter_check(word, letter):
+    for i in word:
+        if i == letter:
+            return True
+        else:
+            return False
+
+print(letter_check('abracadabra', 'a'))
+print(letter_check('abracadabra', 'z'))
+
+#"""
+
+"""
+
+def contains(big_string, little_string):
+    return little_string in big_string
+       
+def common_letters(string_one, string_two):
+    common = []
+    for letter in string_one:
+        if (letter in string_two) and not (letter in common):
+            common.append(letter)
+    return common
+
+print(common_letters("welcome to the jungle", "An apple a day"))
+
+#"""
+
+#"""
+#REVIEW
+
+def username_generator(first_name, last_name):
+    if len(first_name) < 3:
+        username = first_name
+    else:
+        username = first_name[:3]
+    if len(last_name) < 4:
+        username += last_name
+    else:
+        username += last_name[:4]
+    
+    return username
+
+print(username_generator('Patrick', 'Myers'))
+
+def password_generator(username):
+    password = ''
+    for i in range(0, len(username)):
+        password += username[i-1]
+    return password
+
+print(password_generator(username_generator('Wolfgang', 'Marin')))
 
 #"""
